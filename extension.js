7b685fb28ef5e1ba42120d61b0f15673e5f9ab67
Jiwon Lee <jiwonlee@Jiwons-MacBook-Pro.local>
2023-02-23 22:47:29 -0800

    Implement next and prev commands

--- extension.js
+++ extension.js
@@ -8,33 +8,60 @@ const vscode = require('vscode');
  */
 function activate(context) {
 
-	let disposable = vscode.commands.registerCommand('cs347.helloWorld', function () {
+	// Command to go to next bookmark
+	let nextBookmarkCommand = vscode.commands.registerCommand('cs347.nextBookmark', function () {
 		const editor = vscode.window.activeTextEditor;
 
-		if(editor.selection.isEmpty)
-		{
+		if(editor.selection.isEmpty) {
 
-			// display current line number
-			const position = editor.selection.active;
-			console.log(position);
-			vscode.window.showInformationMessage("You are on line:" + String(position.line + 1));
-			
-			// Find comment
+			// Get position (i.e. index) of cursor
+			const cur_position = editor.document.offsetAt(editor.selection.active);
+
+			// Find nearest next bookmark
 			let text = editor.document.getText();
-			const match = text.match(/# !@/);
+			const allMatches = [...text.matchAll(/# !@/g)];
+			const match = allMatches.find((match) => match.index > cur_position);
 
-			console.log(match);
-			var newPosition = editor.document.positionAt(match.index);
-
+			// If match == null, there is no next bookmark; loop around to first
+			var index = (match != null) ? match.index : allMatches[0].index;
+			// console.log(index);
 
 			// Move cursor to that line
+			var newPosition = editor.document.positionAt(index);
+			var newSelection = new vscode.Selection(newPosition, newPosition);
+			editor.selection = newSelection;
+		}
+
+	});
+
+	// Command to go to previous bookmark
+	let prevBookmarkCommand = vscode.commands.registerCommand('cs347.prevBookmark', function () {
+		const editor = vscode.window.activeTextEditor;
+
+		if(editor.selection.isEmpty) {
+
+			// Get position (i.e. index) of cursor
+			const cur_position = editor.document.offsetAt(editor.selection.active);
+
+			// Find nearest previous bookmark
+			let text = editor.document.getText();
+			const allMatches = [...text.matchAll(/# !@/g)].reverse();
+			const match = allMatches.find((match) => match.index < cur_position);
+
+			// If match == null, there is no next bookmark; loop around to last
+			var index = (match != null) ? match.index : allMatches[0].index;
+			// console.log(index);
+
+			// Move cursor to that line
+			var newPosition = editor.document.positionAt(index);
 			var newSelection = new vscode.Selection(newPosition, newPosition);
 			editor.selection = newSelection;
 		}
 		
 	});
 
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(nextBookmarkCommand);
+	context.subscriptions.push(prevBookmarkCommand);
 }
 
 // This method is called when your extension is deactivated
