63c5bbf78c0126f57c0f2bffb58610313a3e79ea
Safiyah Lakhany <safiyahlakhany@Safiyahs-MacBook-Air-2.local>
2023-03-06 10:26:57 -0800

    Added sound functionalitygit add .

--- extension.js
+++ extension.js
@@ -1,4 +1,6 @@
 const vscode = require('vscode');
+const sound = require("sound-play");
+
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -34,9 +36,35 @@ function activate(context) {
 			var newPosition = editor.document.positionAt(index + 4); // 4 == length of bookmark indicator (# !@)
 			var newSelection = new vscode.Selection(newPosition, newPosition);
 			editor.selection = newSelection;
+
+			playNextSound();
+
 		}
 
 	});
+
+	async function playNextSound() {
+		const path = require("path");
+		const filePath = path.join(__dirname, "next_new.m4a");
+		try {
+			await sound.play(filePath);
+			console.log("done");
+		  } catch (error) {
+			console.error(error);
+		  }
+
+	}
+	async function playPrevSound() {
+		const path = require("path");
+		const filePath = path.join(__dirname, "prev_new.m4a");
+		try {
+			await sound.play(filePath);
+			console.log("done");
+		  } catch (error) {
+			console.error(error);
+		  }
+
+	}
 
 	// Command to go to previous bookmark
 	let prevBookmarkCommand = vscode.commands.registerCommand('cs347.prevBookmark', function () {
@@ -64,6 +92,8 @@ function activate(context) {
 			var newPosition = editor.document.positionAt(index + 4); // 4 == length of bookmark indicator (# !@)
 			var newSelection = new vscode.Selection(newPosition, newPosition);
 			editor.selection = newSelection;
+
+			playPrevSound();
 		}
 		
 	});
